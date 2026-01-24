b4071c5c549f9c3e90688d61aa2f82100b032fbc
Gebertwo <lassomoluna9@gmail.com>
2026-01-23 20:40:21 -0500

    Add Product Endpoints

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create_product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly customersService: ProductService) { }
+
+    @Post()
+    create(@Body() dto: CreateProductDto) {
+        return this.customersService.create(dto);
+    }
+
+    @Get()
+    read() {
+        return this.customersService.findAll()
+    }
+}
