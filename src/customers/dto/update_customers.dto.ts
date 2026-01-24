cba2c54f4e650fd3bce8bad08bf3c74397913ee1
Gebertwo <lassomoluna9@gmail.com>
2026-01-23 19:20:34 -0500

    feat(customer.service.ts): Update an improve to make use of DTOs and entities

--- src/customers/dto/update_customers.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCustomerDto } from './create_customers.dto';
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto)
-{ }
