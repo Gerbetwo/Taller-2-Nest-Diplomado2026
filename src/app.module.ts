40cea2cc1c4517d1974d396d47d46b37207ffea3
Gebertwo <lassomoluna9@gmail.com>
2026-01-23 19:15:34 -0500

    feat(Customers): Module to make CRUD to customers, feat(Package.json): Update package to use mapped-types

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
