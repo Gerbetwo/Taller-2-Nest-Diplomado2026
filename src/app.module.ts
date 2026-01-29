bc8924dfca9c88569fd1c989d842e6fe7cf5e5c1
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-01-29 18:56:39 -0500

    feat: Update Prisma To 7.30 version

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { CustomersModule } from './customers/customers.module';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule, CustomersModule, ProductModule],
+  imports: [HealthModule, CustomersModule, ProductModule,
+    ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
