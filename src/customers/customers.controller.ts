b176e68d737bd4cd4feadd28469aefc1873e5cd0
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-01-29 19:25:22 -0500

    feat: Update Customers Module To add persistence in database

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -10,34 +10,34 @@ export class CustomersController {
 
     // POST: function to create customer Customer.create(<CustomerObject)
     @Post()
-    create(@Body() dto: CreateCustomerDto) {
+    async create(@Body() dto: CreateCustomerDto) {
         return this.customersService.create(dto);
     }
     // GET: function to get all customers Customer.findAll()
     @Get()
-    findAll() {
+    async findAll() {
         return this.customersService.findAll()
     }
     // GET: function to get one customer Customer.findOne(<id>)
     @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id: number) {
+    async findOne(@Param('id', ParseIntPipe) id: number) {
         return this.customersService.findOne(id);
     }
     // PATCH: funvcion to create customer Customer.update(<id>, <CustomerObject>)
     @Patch(':id')
-    update( @Param('id', ParseIntPipe) id: number, @Body() dto: UpdateCustomerDto) {
+    async update( @Param('id', ParseIntPipe) id: number, @Body() dto: UpdateCustomerDto) {
         return this.customersService.update(id, dto);
     }
         // PATCH: funvcion to create customer Customer.update(<id>, <CustomerObject>)
     @Put(':id')
-    upgrade( @Param('id', ParseIntPipe) id: number, @Body() dto: UpdateCustomerDto) {
+    async upgrade( @Param('id', ParseIntPipe) id: number, @Body() dto: UpdateCustomerDto) {
         return this.customersService.update(id, dto);
     }
     // DELETE: funvcion to create customer Customer.remove(<id>)
     @Delete(':id')
     @HttpCode(204)
-    remove(@Param('id', ParseIntPipe) id: number) {
-        this.customersService.remove(id);
+    async remove(@Param('id', ParseIntPipe) id: number) {
+        await this.customersService.remove(id);
     }
 
 }
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,47 +1,49 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateCustomerDto } from './dto/create_customer.dto';
 import { UpdateCustomerDto } from './dto/update_customer.dto';
 import { Customer } from './entities/customer.entity';
 @Injectable()
 export class CustomersService {
 
-    private customers: Customer[] = [];
-    private nextId = 1;
-
-    create(dto: CreateCustomerDto): Customer {
-        const newCustomer: Customer = {
-            id: this.nextId++,
-            fullName: dto.fullName,
-            email: dto.email,
-            phone: dto.phone,
-            isActive: true,
-            createdAt: new Date().toISOString(),
-        };
-
-        this.customers.push(newCustomer);
-        return newCustomer;
+    private readonly prisma: PrismaService;
+    // Funcion Asincrona para crear el objeto de prisma
+    async create(dto: CreateCustomerDto) {
+        return this.prisma.customer.create({
+            data: {
+                fullName: dto.fullName,
+                email: dto.email,
+                phone: dto.phone,
+            },
+        });
     }
+
     // Obtener todos los registros de la base de datos Customers
-    findAll(): Customer[] {
-        return this.customers;
+    async findAll() {
+        return this.prisma.customer.findMany({
+            orderBy: { id: 'asc' },
+        });
     }
+
     // Obtener un registro de la base de datos Customers: Customer: <ID>
-    findOne(id: number): Customer {
-    const found = this.customers.find(c => c.id === id);
-        if (!found) throw new NotFoundException(`Customer ${id} no existe`);
-        return found;
-    }
-    // Modificar o actualizar un registro de la base de datos Customers: Customer:[<ID>, <CustomerObject>]
-    update(id: number, dto: UpdateCustomerDto): Customer {
-        const customer = this.findOne(id);
-        Object.assign(customer, dto);
+    async findOne(id: number) {
+        const customer = await this.prisma.customer.findUnique({ where: { id } });
+        if (!customer) throw new NotFoundException(`Customer ${id} no existe`);
         return customer;
     }
 
+    // Modificar o actualizar un registro de la base de datos Customers: Customer:[<ID>, <CustomerObject>]
+    async update(id: number, dto: UpdateCustomerDto) {
+        await this.findOne(id); // asegura 404 si no existe
+        return this.prisma.customer.update({
+            where: { id },
+            data: dto,
+        });
+    }
+
     // Eliminar un registro de la base de datos Customers: Customer: <ID>
-    remove(id: number): void {
-        const idx = this.customers.findIndex(c => c.id === id);
-        if (idx === -1) throw new NotFoundException(`Customer ${id} no existe`);
-        this.customers.splice(idx, 1);
+    async remove(id: number) {
+        await this.findOne(id);
+        await this.prisma.customer.delete({ where: { id } });
     }
 }
