7b41aac36feafbbb688cb61992f9cb1134859f11
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-01-29 20:26:38 -0500

    fix(prisma): Fix autoincrement -> autoincrement()

---
+++ prisma/migrations/20260130012544_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "tag" TEXT,
+    "isActive" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
