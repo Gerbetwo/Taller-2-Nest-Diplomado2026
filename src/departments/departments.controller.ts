77a5b78aeff2d2f5e986745c32b13bf064a4466e
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-01-30 19:11:54 -0500

    feat(module): Update features to  Modules Deparments, Student, Courses

--- src/departments/departments.controller.ts
+++ src/departments/departments.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { DepartmentsService } from './departments.service';
 @Controller('departments')
-export class DepartmentsController {}
+export class DepartmentsController {
+    constructor(private readonly service: DepartmentsService) { }
+    
+    @Post()
+    async create(@Body() body: { name: string }) {
+    return this.service.create(body.name);
+    }
+
+    @Get()
+    async findAll() {
+    return this.service.findAll();
+    }
+
+    @Get(':id/courses')
+    async findOneWithCourses(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findOneWithCourses(id);
+    }
+}
