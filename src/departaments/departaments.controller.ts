e55217e6debaf8fca5b92d782fd8621493118f1b
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-02-06 17:25:52 -0500

    fix: Update get function to findOne

--- src/departaments/departaments.controller.ts
+++ src/departaments/departaments.controller.ts
@@ -3,19 +3,24 @@ import { DepartamentsService } from './departaments.service';
 @Controller('departments')
 export class DepartmentsController {
     constructor(private readonly service: DepartamentsService) { }
-    
+
     @Post()
     async create(@Body() body: { name: string }) {
-    return this.service.create(body.name);
+        return this.service.create(body.name);
     }
 
     @Get()
     async findAll() {
-    return this.service.findAll();
+        return this.service.findAll();
+    }
+
+    @Get()
+    async findOne( id ) {
+        return this.service.findOne(id);
     }
 
     @Get(':id/courses')
     async findOneWithCourses(@Param('id', ParseIntPipe) id: number) {
-    return this.service.findOneWithCourses(id);
+        return this.service.findOneWithCourses(id);
     }
 }
